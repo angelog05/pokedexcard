cddcde3e7c6e7a940e6aa3c4182e9d091583225b
Angelo <agarridotobar@gmail.com>
2023-02-09 00:24:07 -0300

    added style to pokemon [id]

--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -3,15 +3,25 @@ import Head from "next/head";
 import fetch from "isomorphic-fetch";
 
 const Pokemon = ({ pokemon }) => {
+  const hp = pokemon.stats.filter((item) => item.stat.name === "hp");
+
   return (
     <div>
       <Head>
         <title>{pokemon.name.toUpperCase()}</title>
       </Head>
       <Layout>
-        <section className="md:flex bg-slate-100 rounded-xl" style={{ border: ''}}>
-          <h1>{pokemon.name.toUpperCase()}</h1>
-          <img src={`${pokemon.sprites.other.home.front_default}`} />
+        <section className="md:flex bg-slate-100 rounded-lg bg-primary-50 pl-4 pr-4 pt-2 pb-3">
+          <article className="flex justify-between items-baseline">
+            <h1 className="font-bold">{pokemon.name.toUpperCase()}</h1>
+            <aside className="stats flex items-baseline">
+              <h1 className="text-xs">{hp[0].stat.name}</h1>
+              <h1 className="text-primary-900 text-xl font-semibold">{hp[0].base_stat}</h1>
+            </aside>
+          </article>
+          <article className="bg-primary-300">
+            <img src={`${pokemon.sprites.other.home.front_default}`} />
+          </article>
         </section>
       </Layout>
     </div>
@@ -22,11 +32,12 @@ Pokemon.getInitialProps = async (ctx) => {
   const pokemon = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${ctx.query.id}`
   ).then(async (poke) => {
-    const { name, sprites } = await poke.json();
+    const { name, sprites, stats } = await poke.json();
 
     return {
       name,
       sprites,
+      stats,
     };
   });
 
