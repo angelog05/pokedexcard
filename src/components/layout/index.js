12a8af96ad0ba202c62a2e00d04067502c6c65dc
Angelo Garrido <angelo.garrido@23people.io>
2023-03-08 18:21:08 -0300

    Added timeout by loading

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,10 +2,12 @@ import Nav from "../nav";
 
 const Layout = (props) => {
   return (
-    <div className="pl-3 pr-3 pb-6 bg-primary-400 h-full">
-      <Nav />
-      <div className="flex justify-center py-4">{props.children}</div>
-      {/* <Footer /> */}
+    <div className="h-screen bg-primary-400">
+      <div className="pl-3 pr-3 pb-6 bg-primary-400 ">
+        <Nav />
+        <div className="flex justify-center py-4">{props.children}</div>
+        {/* <Footer /> */}
+      </div>
     </div>
   );
 };
--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -1,21 +1,27 @@
 import fetch from "isomorphic-fetch";
-import { useRouter } from 'next/router';
 import Head from "next/head";
+import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
-import ErrorNotFound from "../../components/error-not-found";
 import Layout from "../../components/layout";
 import Movements from "../../components/movements";
 
 const Pokemon = ({ pokemon }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  
   useEffect(() => {
+    console.log('effect')
+    setIsLoading(true);
     if (pokemon.error) {
       setError(true);
       router.push("/not-found")
     } else {
       setError(false);
     }
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
   }, [pokemon]);
 
   const hp = !pokemon.error
@@ -35,10 +41,11 @@ const Pokemon = ({ pokemon }) => {
         <title>{pokemon?.name.toUpperCase()}</title>
       </Head>
 
-      <Layout>
-        {error && <ErrorNotFound />}
-        {!error && (
-          <section className="pokemon sm:w-3/4 xl:w-3/5 bg-slate-100 rounded-lg bg-primary-50 pl-4 pr-4 pt-2 pb-3 mb-20">
+      <Layout> 
+        {isLoading && <div>Loading</div>}     
+
+        {!isLoading && (
+          <section className="pokemon max-w-sm sm:w-2/4 xl:w-2/5 bg-slate-100 rounded-lg bg-primary-50 pl-4 pr-4 pt-2 pb-3 mb-20">
             <article className="flex justify-between items-baseline">
               <h1 className="font-bold text-lg">
                 {pokemon?.name.toUpperCase()}
